33bd48cdbe0b69f2f927bb25ec1357872ee2912e
Liam <40115865@live.napier.ac.uk>
2017-01-27 19:34:56 +0000

    RecyclerView implemented on CardView added to technique fragment

--- app/src/main/java/com/example/liamk/version3/TechFrag.java
+++ app/src/main/java/com/example/liamk/version3/TechFrag.java
@@ -2,6 +2,8 @@ package com.example.liamk.version3;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +14,17 @@ import android.view.ViewGroup;
 public class TechFrag extends Fragment{
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        return inflater.inflate(R.layout.techfrag, container, false);
+        View view = inflater.inflate(R.layout.techfrag, container, false);
+
+       RecyclerView rv = (RecyclerView) view.findViewById(R.id.techRecycle);
+        rv.setHasFixedSize(true);
+        MyAdapter adapter = new MyAdapter(new String[]{"Take a time out", "Countdown", "Meditation", "Positive Reinforcement", "4-7-8 Breathing" , "Breath Regulation" , "Aerobic Exercise", "Open Up"});
+        rv.setAdapter(adapter);
+
+        LinearLayoutManager llm = new LinearLayoutManager(getActivity());
+        rv.setLayoutManager(llm);
+
+        return view;
     }
+
 }
